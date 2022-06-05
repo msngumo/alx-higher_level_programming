415b3ddf5ee40539ddaea6922d272590b1de92a6
joyngumo <joyngumo@gmail.com>
2022-06-05 17:23:35 +0300

    00..99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for x in range(0, 100):
-    if x < 10:
-        print("0{},".format(x), end=" ")
-    elif x == 99:
-        print("{}".format(x), end="")
+    if x != 99:
+        print("{:02d},".format(x), end=" ")
     else:
-        print("{},".format(x), end=" ")
+        print("{}".format(x))
