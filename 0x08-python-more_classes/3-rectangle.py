d7d6c5fd15aa603dae30d35a2b4f8a3b7b5412dd
joyngumo <joyngumo@gmail.com>
2022-06-30 20:33:35 +0300

    String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"Defines a class"
+
+
+class Rectangle:
+    "Defines a rectangle"
+    def __init__(self, width=0, height=0):
+        "instantiates width and height"
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        "defining area"
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (2 * self.__width) + (2 * self.__height)
+
+    def __str__(self):
+        new_str = ""
+        if self.__width == 0 or self.__height == 0:
+            return new_str
+        else:
+            for x in range(self.__height):
+                for y in range(self.__width):
+                    new_str += "#"
+                if x != self.__height - 1:
+                    new_str += "\n"
+            return new_str
