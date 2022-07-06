432baa837caf63b5018d74f032b6163b1fd143cf
joyngumo <joyngumo@gmail.com>
2022-07-06 05:54:12 +0300

    i/o

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""1-my_list module"""
+
+
+class MyList(list):
+    """child class of parent class list"""
+
+    def print_sorted(self):
+        "prints list in ascending order"
+
+        print(sorted(list))
