e7a26896389d15135038199392972bc66ec37c39
joyngumo <joyngumo@gmail.com>
2022-08-10 00:48:26 +0300

    print description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- prints full description first_table from the database hbtn_0c
 -- DB name will be passed as an argument of the mysql command
 
-SELECT * FROM hbtn_0c_0.first_table;
+SELECT first_table FROM hbtn_0c_0;
