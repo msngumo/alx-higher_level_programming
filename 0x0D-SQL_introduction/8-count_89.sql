6ff6a7028696642fb962f13f32b8e8ea4eecc255
joyngumo <joyngumo@gmail.com>
2022-08-10 23:28:28 +0300

    count.

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- displays the number of records with id = 89 in first_table 
 -- database name will be passed as an argument of the mysql command
 
-SELECT COUNT * FROM first_table WHERE `id` = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
