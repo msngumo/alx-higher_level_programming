0f1ed0d9d8d1cce1e985021e90d5f6df1cdd151f
joyngumo <joyngumo@gmail.com>
2022-08-11 00:55:39 +0300

    no null.

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -3,4 +3,4 @@
 -- Results should display the score and the name (in this order)
 -- Records should be listed by descending score
 
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != " " ORDER BY score DESC;
