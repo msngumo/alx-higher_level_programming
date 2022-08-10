2eddafe7dd7199b72b66afe2984b2f32414537db
joyngumo <joyngumo@gmail.com>
2022-08-10 23:50:31 +0300

    top score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table of the database
+-- Results should display both score and name (in this order)
+-- Records should be ordered by score (top first)
+
+SELECT score, name FROM second_table ORDER BY score DESC
